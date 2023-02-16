c8fdad61eaeb6c890ce9a92f79933a5cd74192e3
IvarGorenko <73782886+IvarGorenko@users.noreply.github.com>
2023-02-16 23:45:57 +0100

    fundamental structure completed

--- nodelink.js
+++ nodelink.js
@@ -1,26 +1,8 @@
-var width = 600, height = 400
+var width = 800, height = 500
+var clickState = 0;
 
-var nodes = [
-	{name: 'A'},
-	{name: 'B'},
-	{name: 'C'},
-	{name: 'D'},
-	{name: 'E'},
-	{name: 'F'},
-	{name: 'G'},
-	{name: 'H'},
-]
-
-var links = [
-	{source: 0, target: 1},
-	{source: 0, target: 2},
-	{source: 0, target: 3},
-	{source: 1, target: 6},
-	{source: 3, target: 4},
-	{source: 3, target: 7},
-	{source: 4, target: 5},
-	{source: 4, target: 7}
-]
+var nodes = [];
+var links = [];
 
 var nodes1 = [
 	{name: 'A'},
@@ -30,7 +12,7 @@ var nodes1 = [
 	{name: 'E'},
 	{name: 'F'},
 	{name: 'G'},
-	{name: 'H'},
+	{name: 'Hdsadf'},
 ]
 
 var links1 = [
@@ -44,17 +26,26 @@ var links1 = [
 	{source: 4, target: 7}
 ]
 
-var simulation = d3.forceSimulation(nodes)
-	.force('charge', d3.forceManyBody().strength(-100))
+var starWarsData = "starwars";
+
+d3.json("/starwars-interactions/starwars-episode-1-interactions-allCharacters.json",).then(function(data){
+	starWarsData = data;
+	console.log(starWarsData);
+	nodes = starWarsData.nodes;
+	links = starWarsData.links;
+	var simulation = d3.forceSimulation(nodes)
+	.force('charge', d3.forceManyBody().strength(-300))
 	.force('center', d3.forceCenter(width / 2, height / 2))
 	.force('link', d3.forceLink().links(links))
 	.on('tick', ticked);
+});
 
-    var simulation1 = d3.forceSimulation(nodes1)
-	.force('charge', d3.forceManyBody().strength(-100))
-	.force('center', d3.forceCenter(width / 2, height / 2))
-	.force('link', d3.forceLink().links(links1))
-	.on('tick', ticked);
+
+var simulation1 = d3.forceSimulation(nodes1)
+.force('charge', d3.forceManyBody().strength(-100))
+.force('center', d3.forceCenter(width / 2, height / 2))
+.force('link', d3.forceLink().links(links1))
+.on('tick', ticked);
 
 function updateLinks() {
 	var u = d3.select('.links')
@@ -74,7 +65,7 @@ function updateLinks() {
 			return d.target.y
 		});
 
-        var v = d3.select('.links1')
+    var v = d3.select('.links1')
 		.selectAll('line')
 		.data(links1)
 		.join('line')
@@ -94,23 +85,43 @@ function updateLinks() {
 
 function updateNodes() {
 	u = d3.select('.nodes')
-		.selectAll('text')
+		.selectAll('circle')
 		.data(nodes)
-		.join('text')
-		.text(function(d) {
-			return d.name
+		.join('circle')
+		.attr('r', function(d){
+			return d.value*0.5;
 		})
-		.attr('x', function(d) {
+		.attr('fill',function(d){
+			return d.colour
+		})
+		.attr('cx', function(d) {
 			return d.x
 		})
-		.attr('y', function(d) {
+		.attr('cy', function(d) {
 			return d.y
 		})
-		.attr('dy', function(d) {
-			return 5
+		.attr('name', function(d) {
+			return d.name
+		})
+
+		u.on("click", function() {
+			var clickedNode = d3.select(this).attr("name")
+			console.log("Clicked: " + clickedNode);
+			console.log(clickState)
+			u.attr("opacity", function(d) {
+				if (clickState == 0) { // TODO: states funkar inte...
+					clickState = 1;
+					return (d.name == clickedNode) ? 1 : 0.3;
+				} 
+				else {
+					clickState = 0;
+					return 1;
+				}
+					
+			});
 		});
 
-        v = d3.select('.nodes1')
+    v = d3.select('.nodes1')
 		.selectAll('text')
 		.data(nodes1)
 		.join('text')
